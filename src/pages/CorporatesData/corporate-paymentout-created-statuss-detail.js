bb86714f61f2b6d0b5e6adaf73b67910cbefd372
labhazir <95848346+labhazir@users.noreply.github.com>
2024-09-07 15:44:38 +0500

    page layout deploy

--- src/pages/CorporatesData/corporate-paymentout-created-statuss-detail.js
+++ src/pages/CorporatesData/corporate-paymentout-created-statuss-detail.js
@@ -3,10 +3,13 @@ import PropTypes from "prop-types";
 import axios from "axios";
 import MetaTags from "react-meta-tags";
 import { withRouter } from "react-router-dom";
-import { Card, CardBody, Col, Container, Row, Button } from "reactstrap";
+import { Card, CardBody, Col, Container, Row, Button, Table } from "reactstrap";
 import Breadcrumbs from "components/Common/Breadcrumb";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
+import { isEmpty, map, split } from "lodash";
+import moment from 'moment';
+
 
 const PaymentStatussList = (props) => {
   const [paymentStatuss, setPaymentStatuss] = useState({});
@@ -16,7 +19,7 @@ const PaymentStatussList = (props) => {
   useEffect(() => {
     const fetchPaymentStatuss = async () => {
       try {
-        const response = await axios.get(`https://labhazirapi.com/api/corporate/corporate-payment-detail/${id}`);
+        const response = await axios.get(`http://127.0.0.1:8000/api/corporate/corporate-payment-detail/${id}`);
         setPaymentStatuss(response.data);
       } catch (error) {
         console.error("Error fetching payment details:", error);
@@ -27,6 +30,10 @@ const PaymentStatussList = (props) => {
   }, [id]);
 
   const paymentDetail = paymentStatuss.data ? paymentStatuss.data[0] : {};
+
+  useEffect(() => {
+    console.log("Payment Detail:", paymentDetail);
+  }, [paymentDetail]);
 
   const handlePrint = () => {
     window.print();
@@ -61,7 +68,8 @@ const PaymentStatussList = (props) => {
         <title>Payments Detail | Lab Hazir</title>
       </MetaTags>
       <Container fluid>
-        <Breadcrumbs title="List" breadcrumbItem="Payments" />
+        <Breadcrumbs title="Payment" breadcrumbItem="Detail" />
+
         <Row>
           <Col lg="12">
             <Card>
@@ -69,16 +77,108 @@ const PaymentStatussList = (props) => {
                 <div ref={printRef}>
                   {paymentDetail ? (
                     <div>
-                      <p><strong>Corporate:</strong> {paymentDetail.corporate_id}</p>
-                      <p><strong>Payment Form ID:</strong> {paymentDetail.id}</p>
-                      <p><strong>Payment For:</strong> {paymentDetail.payment_for}</p>
-                      <p><strong>Payment Date:</strong> {paymentDetail.payment_at}</p>
-                      <p><strong>Amount:</strong> {paymentDetail.amount}</p>
-                      <p><strong>Lab ID:</strong> {paymentDetail.lab_id}</p>
-                      <p><strong>Appointments ID:</strong> {paymentDetail.test_appointment_id}</p>
-                      <p><strong>Payment Method:</strong> {paymentDetail.payment_method}</p>
-                      <p><strong>Payment Status:</strong> {paymentDetail.payment_status}</p>
-                      <p><strong>Comments:</strong> {paymentDetail.comments}</p>
+                      <Row>
+                        <Col sm="4" className="mt-3">
+                          <address>
+                            <strong>Corporate ID: </strong>
+                            <span className="text-danger">{paymentDetail.corporate_id}</span> 
+                          </address>
+                        </Col>
+                        <Col sm="4" className="mt-3">
+                          <address>
+                            <strong>Payment Form ID: </strong>
+                            <span className="text-danger">{paymentDetail.id}</span> 
+                          </address>
+                        </Col>
+                      </Row>
+                      <Row>
+                      
+                        <Col sm="4" className="mt-3">
+                          <address>
+                            <strong>Amount: </strong>
+                            <span className="text-danger">{paymentDetail.amount}</span> 
+                          </address>
+                        </Col>
+                        <Col sm="4" className="mt-3">
+                          <address>
+                            <strong>Payment Method: </strong>
+                            <span className="text-danger" style={{ marginRight: '5px' }}>{paymentDetail.payment_method}</span> 
+                          </address>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col sm="4" className="mt-3">
+                          <address>
+                            <strong>Payment For: </strong>
+                            <span className="text-danger">{paymentDetail.payment_for}</span> 
+                          </address>
+                        </Col>
+                        <Col sm="4" className="mt-3">
+                          <address>
+                            <strong>Payment Date: </strong>
+                            {/* <br /> */}
+                            <span className="text-danger" style={{ marginRight: '5px' }}>{paymentDetail.payment_at}</span> 
+                            {/* <br /> */}
+                          </address>
+                        </Col>
+                      </Row>
+                      <div className="py-2 mt-3">
+                        <h3 className="font-size-15 font-weight-bold">Detail of Appointment</h3>
+                      </div>
+                      <div className="table-responsive">
+                        <Table className="table-nowrap">
+                          <thead>
+                            <tr>
+                              <th className="text-start">Lab Name</th>
+                              <th className="text-start">Type</th>
+                              <th className="text-start">Employee Name</th>
+                              <th className="text-start">Employee Type</th>
+                              <th className="text-start">Appointment ID</th>
+                              <th className="text-start">Lab Payable</th>
+                              <th className="text-start">Labhazir Payable</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {paymentDetail.items && paymentDetail.items.length > 0 ? (
+                              paymentDetail.items.map((item, key) =>
+                                item.test_appointment_id ? item.test_appointment_id.split(',').map((appointment_id, subKey) => (
+                                  <tr key={`${key}-${subKey}`}>
+                                    <td className="text-start">{item.lab_name}</td>
+                                    <td className="text-start">{item.payment_for}</td>
+                                    <td className="text-start">{item.employee_name || ''}</td>
+                                    <td className="text-start">{item.employee_type || ''}</td>
+                                    <td className="text-start">{appointment_id.trim()}</td>
+                                    <td className="text-start">
+                                      {item.payment_for.toLowerCase() === 'lab' ? item.amount : '0'}
+                                    </td>
+                                    <td className="text-start">
+                                      {item.payment_for.toLowerCase() === 'labhazir' ? item.amount : '0'}
+                                    </td>
+                                  </tr>
+                                )) : null
+                              )
+                            ) : (
+                              paymentDetail.test_appointment_id ? paymentDetail.test_appointment_id.split(',').map((appointment_id, key) => (
+                                <tr key={key}>
+                                  <td className="text-start">{paymentDetail.lab_name || ''}</td>
+                                  <td className="text-start">{paymentDetail.payment_for || ''}</td>
+                                  <td className="text-start">{paymentDetail.employee_name || ''}</td>
+                                  <td className="text-start">{paymentDetail.employee_type || ''}</td>
+                                  <td className="text-start">{appointment_id.trim() || ''}</td>
+                                  <td className="text-start">
+                                    {paymentDetail.payment_for.toLowerCase() === 'lab' ? paymentDetail.amount : '0'}
+                                  </td>
+                                  <td className="text-start">
+                                    {paymentDetail.payment_for.toLowerCase() === 'labhazir' ? paymentDetail.amount : '0'}
+                                  </td>
+                                </tr>
+                              )) : null
+                            )}
+                          </tbody>
+                        </Table>
+
+
+                      </div>
                     </div>
                   ) : (
                     <p>No payment details available.</p>
@@ -88,6 +188,7 @@ const PaymentStatussList = (props) => {
                 <Button color="secondary" onClick={handleDownload} style={{ marginLeft: "10px" }}>Download PDF</Button>
               </CardBody>
             </Card>
+
           </Col>
         </Row>
       </Container>
